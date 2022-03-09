9a763fcc6601d8f995a03939e3343202250b3ff7
luohao <hao.luo@hanglok-tech.cn>
2022-03-09 18:45:49 +0800

    fix bug

--- DataPreparation/DownLoadDataFrom100Sever.py
+++ DataPreparation/DownLoadDataFrom100Sever.py
@@ -7,12 +7,15 @@ def GetCtSourceListFromMongo(ip:str, port:int):
     ct_source_name_list = []
     ftp_file_name_list = []
     client=MongoClient(ip,port)  
-    for db in client.list_database_names():
-        if len(db)<35:
+    for db_name in client.list_database_names():
+        if len(db_name)<35:
             continue
+        db = client[db_name]
         collection_ctsource = db['ct_source']
         collection_seg = db['ct_result']
-        ct_source_name = list(collection_ctsource.find({}))[0]['data_name'] + list(collection_ctsource.find({}))[0]['data_name']
+        if (len(list(collection_ctsource.find({})))) == 0 or (len(list(collection_seg.find({}))) == 0):
+            continue
+        ct_source_name = list(collection_ctsource.find({}))[0]['data_name']
         ftp_ctsource_file_name = list(collection_ctsource.find({}))[0]['file_path']
         ftp_ctseg_file_name = list(collection_seg.find({}))[0]['file_path']
         if ct_source_name in ct_source_name_list:
@@ -28,8 +31,8 @@ if __name__ == '__main__':
     port = 21                                  # ftp端口
     usrname = 'hanglok-ftp'       # 登陆用户名
     pwd = 'hanglok'       # 登陆密码
-    ftpath = '/standard/v7/daily/00Z-23Z/201403/'  # 远程文件夹
-    localpath = 'D:/data/GSMaP/'  
+    ftpath = '/home/hanglok-ftp/'  # 远程文件夹
+    localpath = 'C:\luohao\SphereSegDATA/'  
 
     ctsource_names, ftp_files = GetCtSourceListFromMongo("192.168.0.100", 27017)
     
@@ -40,7 +43,6 @@ if __name__ == '__main__':
         localCTpath = localpath + name + r'/'
         if not os.path.exists(localCTpath):
             os.makedirs(localCTpath)
-
         Ftp.downloadFile(ftpath + ftpf[0], localCTpath + ftpf[0])
         Ftp.downloadFile(ftpath + ftpf[1], localCTpath + ftpf[1])
         print(name, " DownLoaded")
